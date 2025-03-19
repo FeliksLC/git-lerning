c9c6330c8bf2cda039167d615c8d4ae2ad0dad6f
Omar Langustas Crabos <OmarLC@yandex.ru>
2025-03-19 16:40:09 +0300

    delete "LEARNING"

--- src/main/java/by/felix/GitNN.java
+++ src/main/java/by/felix/GitNN.java
@@ -3,7 +3,6 @@ package by.felix;
 public class GitNN {
     public static void main(String[] args) {
         System.out.println("git DEVELOP");
-        System.out.println("git-LEARNING!!!");
         System.out.println("It`s terribly difficult");
         System.out.println("Im happy");
         System.out.println("!!!");
