814b0a511c39a7e1ac8f4a0b4823d09273a8b482
FeliksLC <131269016+FeliksLC@users.noreply.github.com>
2025-03-19 11:27:27 +0300

    Update GitNN.java
    Add symbol "!" to sout "git-LEARNING"

--- src/main/java/by/felix/GitNN.java
+++ src/main/java/by/felix/GitNN.java
@@ -3,6 +3,6 @@ package by.felix;
 public class GitNN {
     public static void main(String[] args) {
         System.out.println("git");
-        System.out.println("git-LEARNING");
+        System.out.println("git-LEARNING!!!");
     }
 }
