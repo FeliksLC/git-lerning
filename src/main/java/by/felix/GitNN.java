5be11add3366768013a66e92aee85a5664d95e47
Omar Langustas Crabos <OmarLC@yandex.ru>
2025-03-19 17:26:28 +0300

    add sout "bueeess"

--- src/main/java/by/felix/GitNN.java
+++ src/main/java/by/felix/GitNN.java
@@ -4,7 +4,7 @@ public class GitNN {
     public static void main(String[] args) {
         System.out.println("git DEVELOP");
         System.out.println("git-LEARNING!!!");
-        System.out.println("bueee");
+        System.out.println("bueeess");
         System.out.println("Im happy");
         System.out.println("?????");
     }
