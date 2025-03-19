11abd99d043ae2d4c17149e77b47102c7544e4f8
Omar Langustas Crabos <OmarLC@yandex.ru>
2025-03-19 14:11:26 +0300

    Change sout "git" + "DEVELOP"

--- src/main/java/by/felix/GitNN.java
+++ src/main/java/by/felix/GitNN.java
@@ -2,7 +2,7 @@ package by.felix;
 
 public class GitNN {
     public static void main(String[] args) {
-        System.out.println("git");
+        System.out.println("git DEVELOP");
         System.out.println("git-LEARNING!!!");
         System.out.println("It`s terribly difficult");
         System.out.println("Im happy");
