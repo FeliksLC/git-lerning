bd580da58802e49bfbc77cd7e0f443af04fe237e
Omar Langustas Crabos <OmarLC@yandex.ru>
2025-03-19 16:57:05 +0300

    delete sout

--- src/main/java/by/felix/GitNN.java
+++ src/main/java/by/felix/GitNN.java
@@ -3,8 +3,8 @@ package by.felix;
 public class GitNN {
     public static void main(String[] args) {
         System.out.println("git DEVELOP");
-        System.out.println("It`s terribly difficult");
-        System.out.println("Im Srrrrr happy");
+        System.out.println("git-LEARNING!!!");
+        System.out.println("Im happy");
         System.out.println("!!!");
     }
 }
