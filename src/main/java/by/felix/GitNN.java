84ca4c6e5ee2475f8833d88f05c9a0cced57ecf9
FeliksLC <131269016+FeliksLC@users.noreply.github.com>
2025-03-19 11:34:22 +0300

    Update GitNN.java
    add sout "Im happy"

--- src/main/java/by/felix/GitNN.java
+++ src/main/java/by/felix/GitNN.java
@@ -4,5 +4,6 @@ public class GitNN {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("git-LEARNING!!!");
+        System.out.println("Im happy");
     }
 }
